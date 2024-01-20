f606f1a3ea097ff115ce408e28c39589899ab7f7
alltidsemester <alltidsemester+git@pm.me>
2024-01-20 20:02:56 +0100

    refactor ui

--- src/ui.rs
+++ src/ui.rs
@@ -6,36 +6,63 @@ use ratatui::text::Text;
 use ratatui::widgets::Block;
 use ratatui::widgets::Borders;
 use ratatui::widgets::Paragraph;
+use ratatui::widgets::StatefulWidget;
 use ratatui::Frame;
 
-pub(crate) fn ui(frame: &mut Frame, screen: &Screen) {
-    let main_ui = Paragraph::new(main_ui_lines(screen)).scroll((screen.scroll, 0));
-
-    if let Some(ref cmd) = screen.command {
-        let output_lines = format_command(cmd);
-
-        let layout = Layout::new(
-            Direction::Vertical,
-            [
-                Constraint::Min(1),
-                Constraint::Length(output_lines.len() as u16 + 1),
-            ],
-        )
-        .split(frame.size());
-
-        frame.render_widget(main_ui, layout[0]);
-        frame.render_widget(command_popup(output_lines), layout[1]);
+pub(crate) fn ui(frame: &mut Frame, screen: &mut Screen) {
+    let cmd_text = if let Some(ref cmd) = screen.command {
+        format_command(cmd)
     } else {
-        frame.render_widget(main_ui, frame.size());
+        vec![]
+    };
+
+    let cmd_len = cmd_text.len() as u16;
+    let layout = Layout::new(
+        Direction::Vertical,
+        [
+            Constraint::Min(1),
+            Constraint::Length(if cmd_len > 0 { cmd_len + 1 } else { 0 }),
+        ],
+    )
+    .split(frame.size());
+
+    frame.render_stateful_widget(ScreenWidget, layout[0], screen);
+
+    if !cmd_text.is_empty() {
+        frame.render_widget(command_popup(cmd_text), layout[1]);
     }
 }
 
-fn main_ui_lines(screen: &Screen) -> Vec<Line> {
-    let mut highlight_depth = None;
+struct ScreenWidget;
 
+impl StatefulWidget for ScreenWidget {
+    type State = Screen;
+
+    fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
+        for (i, line) in main_ui_lines(&*state)
+            .into_iter()
+            .skip(state.scroll as usize)
+            .take(area.height as usize)
+            .enumerate()
+        {
+            buf.set_line(0, i as u16, &line, area.width);
+        }
+    }
+}
+
+fn main_ui_lines(screen: &Screen) -> impl Iterator<Item = Line> {
     screen
         .collapsed_items_iter()
-        .flat_map(|(i, item)| {
+        .scan(None, |highlight_depth, (i, item)| {
+            if screen.cursor == i {
+                *highlight_depth = Some(item.depth);
+            } else if highlight_depth.is_some_and(|s| s >= item.depth) {
+                *highlight_depth = None;
+            };
+
+            Some((i, item, *highlight_depth))
+        })
+        .flat_map(|(i, item, highlight_depth)| {
             let mut text = if let Some((ref text, style)) = item.display {
                 use ansi_to_tui::IntoText;
                 let mut text = text.into_text().expect("Couldn't read ansi codes");
@@ -62,32 +89,29 @@ fn main_ui_lines(screen: &Screen) -> Vec<Line> {
                 ));
             }
 
-            if screen.cursor == i {
-                highlight_depth = Some(item.depth);
-            } else if highlight_depth.is_some_and(|hd| hd >= item.depth) {
-                highlight_depth = None;
-            }
-
             if highlight_depth.is_some() {
-                for line in &mut text.lines {
-                    for span in &mut line.spans {
-                        if span.style.bg.is_none() {
-                            span.style.bg = Some(if screen.cursor == i {
-                                theme::CURRENT_THEME.highlight
-                            } else {
-                                theme::CURRENT_THEME.dim_highlight
-                            })
-                        }
-                    }
-                }
+                highlight_section(&mut text, screen, i);
             }
 
             text
         })
-        .collect::<Vec<_>>()
 }
 
-fn format_command(cmd: &crate::command::IssuedCommand) -> Vec<Line> {
+fn highlight_section(text: &mut Text<'_>, screen: &Screen, i: usize) {
+    for line in &mut text.lines {
+        for span in &mut line.spans {
+            if span.style.bg.is_none() {
+                span.style.bg = Some(if screen.cursor == i {
+                    theme::CURRENT_THEME.highlight
+                } else {
+                    theme::CURRENT_THEME.dim_highlight
+                })
+            }
+        }
+    }
+}
+
+fn format_command<'a, 'b>(cmd: &'a crate::command::IssuedCommand) -> Vec<Line<'b>> {
     Text::styled(
         format!(
             "$ {}{}",
