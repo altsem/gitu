9090678b4bb5bf4bf8a0c3b7cd05600fea133a56
alltidsemester <alltidsemester+git@pm.me>
2024-01-27 17:23:36 +0100

    refactor: Move Screen widget impl

--- src/screen/mod.rs
+++ src/screen/mod.rs
@@ -1,10 +1,13 @@
+use ratatui::{prelude::*, widgets::Widget};
+
+use crate::theme::CURRENT_THEME;
+
 use super::Item;
 use std::collections::HashSet;
 
 pub(crate) mod log;
 pub(crate) mod show;
 pub(crate) mod status;
-pub(crate) mod widget;
 
 pub(crate) trait ScreenData {
     fn items<'a>(&'a self) -> Vec<Item>;
@@ -166,5 +169,73 @@ impl<'a> Screen {
 
     pub(crate) fn get_selected_item(&self) -> &Item {
         &self.items[self.cursor]
+    }
+
+    fn screen_ui_lines(&self) -> impl Iterator<Item = Line> {
+        self.collapsed_items_iter()
+            .map(|(i, item)| (i, item, get_display_text(&item)))
+            .flat_map(|(i, item, text)| text.lines.into_iter().map(move |line| (i, item, line)))
+            .scan(None, |highlight_depth, (i, item, mut line)| {
+                if self.is_collapsed(&item) {
+                    if line.width() > 0 {
+                        line.spans.push("…".into());
+                    }
+                }
+
+                extend_bg_to_line_end(&mut line, self);
+                if self.cursor == i {
+                    *highlight_depth = Some(item.depth);
+                } else if highlight_depth.is_some_and(|s| s >= item.depth) {
+                    *highlight_depth = None;
+                };
+
+                if highlight_depth.is_some() {
+                    highlight_line(&mut line, self, i);
+                }
+
+                Some((i, item, line))
+            })
+            .map(|(_i, _item, line)| line)
+    }
+}
+
+impl Widget for &Screen {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        self.screen_ui_lines()
+            .skip(self.scroll as usize)
+            .take(area.height as usize)
+            .enumerate()
+            .for_each(|(i, line)| {
+                buf.set_line(0, i as u16, &line, area.width);
+            });
+    }
+}
+
+fn get_display_text<'a>(item: &crate::items::Item) -> Text<'a> {
+    let (ref text, style) = item.display;
+    use ansi_to_tui::IntoText;
+    let mut text = text.into_text().expect("Couldn't read ansi codes");
+    text.patch_style(style);
+    text
+}
+
+fn extend_bg_to_line_end(line: &mut Line<'_>, screen: &Screen) {
+    let padding = (screen.size.0 as usize).saturating_sub(line.width());
+
+    line.spans.push(Span::styled(
+        " ".repeat(padding),
+        line.spans.first().unwrap().style,
+    ));
+}
+
+fn highlight_line(line: &mut Line<'_>, screen: &Screen, i: usize) {
+    for span in &mut line.spans {
+        if span.style.bg.is_none() {
+            span.style.bg = Some(if screen.cursor == i {
+                CURRENT_THEME.highlight
+            } else {
+                CURRENT_THEME.dim_highlight
+            })
+        }
     }
 }
