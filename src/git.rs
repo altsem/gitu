ccc5191fc1aee823c2dc16a65ab12ad5fbadc081
alltidsemester <alltidsemester+git@pm.me>
2024-01-28 18:37:59 +0100

    refactor: helper fn for git

--- src/git.rs
+++ src/git.rs
@@ -1,8 +1,4 @@
-use crate::{
-    diff::{self, Diff},
-    process,
-    status::Status,
-};
+use crate::{diff::Diff, process, status::Status};
 use std::process::Command;
 
 // TODO Check for.git/index.lock and block if it exists
@@ -11,31 +7,24 @@ use std::process::Command;
 pub(crate) fn status() -> Status {
     Status::parse(&process::run(&["git", "status", "--porcelain", "--branch"]).0)
 }
-
 pub(crate) fn status_simple() -> String {
     process::run(&["git", "status"]).0
 }
-
 pub(crate) fn diff_unstaged() -> Diff {
     Diff::parse(&process::run(&["git", "diff"]).0)
 }
-
 pub(crate) fn show(args: &[&str]) -> Diff {
     Diff::parse(&process::run(&[&["git", "show"], args].concat()).0)
 }
-
 pub(crate) fn show_summary(args: &[&str]) -> String {
     process::run(&[&["git", "show", "--summary", "--decorate", "--color"], args].concat()).0
 }
-
 pub(crate) fn diff(args: &[&str]) -> Diff {
     Diff::parse(&process::run(&[&["git", "diff"], args].concat()).0)
 }
-
 pub(crate) fn diff_staged() -> Diff {
     Diff::parse(&process::run(&["git", "diff", "--staged"]).0)
 }
-
 pub(crate) fn log_recent() -> String {
     process::run(&[
         "git",
@@ -54,94 +43,59 @@ pub(crate) fn log(args: &[&str]) -> String {
 }
 
 pub(crate) fn stage_file_cmd(file: &str) -> Command {
-    let mut cmd = Command::new("git");
-    cmd.args(["add", &file]);
-    cmd
+    git(&["add", &file])
 }
-
 pub(crate) fn stage_patch_cmd() -> Command {
-    let mut cmd = Command::new("git");
-    cmd.args(["apply", "--cached"]);
-    cmd
+    git(&["apply", "--cached"])
 }
-
 pub(crate) fn unstage_file_cmd(file: &str) -> Command {
-    let mut cmd = Command::new("git");
-    cmd.args(["restore", "--staged", file]);
-    cmd
+    git(&["restore", "--staged", file])
 }
-
 pub(crate) fn unstage_patch_cmd() -> Command {
-    let mut cmd = Command::new("git");
-    cmd.args(["apply", "--cached", "--reverse"]);
-    cmd
+    git(&["apply", "--cached", "--reverse"])
 }
-
 pub(crate) fn commit_cmd() -> Command {
-    let mut cmd = Command::new("git");
-    cmd.arg("commit");
-    cmd
+    git(&["commit"])
 }
-
 pub(crate) fn commit_amend_cmd() -> Command {
-    let mut cmd = Command::new("git");
-    cmd.args(["commit", "--amend"]);
-    cmd
+    git(&["commit", "--amend"])
 }
-
 pub(crate) fn commit_fixup_cmd(reference: &str) -> Command {
-    let mut cmd = Command::new("git");
-    cmd.args(["commit", "--fixup", reference]);
-    cmd
+    git(&["commit", "--fixup", reference])
 }
-
 pub(crate) fn push_cmd() -> Command {
-    let mut cmd = Command::new("git");
-    cmd.arg("push");
-    cmd
+    git(&["push"])
 }
-
 pub(crate) fn pull_cmd() -> Command {
-    let mut cmd = Command::new("git");
-    cmd.arg("pull");
-    cmd
+    git(&["pull"])
 }
-
 pub(crate) fn fetch_all_cmd() -> Command {
-    let mut cmd = Command::new("git");
-    cmd.args(["fetch", "--all"]);
-    cmd
+    git(&["fetch", "--all"])
 }
-
 pub(crate) fn rebase_interactive_cmd(reference: &str) -> Command {
-    let mut cmd = Command::new("git");
     // TODO autostash flag should be visible as a flag (though set as default)
-    cmd.args(["rebase", "-i", "--autostash", reference]);
-    cmd
+    git(&["rebase", "-i", "--autostash", reference])
 }
-
 pub(crate) fn rebase_autosquash_cmd(reference: &str) -> Command {
-    let mut cmd = Command::new("git");
     // TODO autostash flag should be visible as a flag (though set as default)
-    cmd.args([
+    git(&[
         "rebase",
         "-i",
         "--autosquash",
         "--keep-empty",
         "--autostash",
         reference,
-    ]);
-    cmd
+    ])
 }
-
 pub(crate) fn rebase_continue_cmd() -> Command {
-    let mut cmd = Command::new("git");
-    cmd.args(["rebase", "--continue"]);
-    cmd
+    git(&["rebase", "--continue"])
+}
+pub(crate) fn rebase_abort_cmd() -> Command {
+    git(&["rebase", "--abort"])
 }
 
-pub(crate) fn rebase_abort_cmd() -> Command {
+fn git(args: &[&str]) -> Command {
     let mut cmd = Command::new("git");
-    cmd.args(["rebase", "--continue"]);
+    cmd.args(args);
     cmd
 }
