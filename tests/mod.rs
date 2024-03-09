e3b5e65b94a00bdedfd788b5418c2e12c1a71801
altsem <alltidsemester@pm.me>
2024-03-09 10:20:21 +0100

    test: add go_down_past_collapsed test

--- tests/mod.rs
+++ tests/mod.rs
@@ -355,3 +355,18 @@ fn stage_last_hunk_of_first_delta() {
         .unwrap();
     insta::assert_snapshot!(ctx.redact_buffer());
 }
+
+#[test]
+fn go_down_past_collapsed() {
+    let mut ctx = TestContext::setup_init(80, 20);
+    commit(ctx.dir.path(), "file-one", "asdf\nblahonga\n");
+    commit(ctx.dir.path(), "file-two", "FOO\nBAR\n");
+    fs::write(ctx.dir.child("file-one"), "blahonga\n").unwrap();
+    fs::write(ctx.dir.child("file-two"), "blahonga\n").unwrap();
+
+    let mut state = ctx.init_state();
+    state
+        .update(&mut ctx.term, &[key('j'), key('j'), key('j')])
+        .unwrap();
+    insta::assert_snapshot!(ctx.redact_buffer());
+}
