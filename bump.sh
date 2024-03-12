44ff8b6eb614bb62c6de86661282b4f6ac297980
altsem <alltidsemester@pm.me>
2024-03-12 21:10:39 +0100

    chore: update bump.sh to write changelog entry message

--- bump.sh
+++ bump.sh
@@ -11,7 +11,8 @@ fi
 
 cargo set-version "$(echo "$version" | sed s/^v//)"
 git cliff --tag "$version" > CHANGELOG.md
+changelog_entry="$(git cliff --unreleased --tag "$version" --strip header)"
 
 git add Cargo.toml Cargo.lock CHANGELOG.md
 git commit -m "chore(release): prepare for $version"
-git -c core.commentChar="@" tag -am "$(git cliff --latest --strip header)" "$version"
+git -c core.commentChar="@" tag -am "$changelog_entry" "$version"
