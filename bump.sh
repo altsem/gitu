4ab7a8f83816d8b8a2ac5bea2e6790b80ccec878
altsem <alltidsemester@pm.me>
2024-03-12 20:43:36 +0100

    fix: include changelog entry in github release

--- bump.sh
+++ bump.sh
@@ -9,4 +9,4 @@ git cliff --tag "$version" > CHANGELOG.md
 
 git add Cargo.toml Cargo.lock CHANGELOG.md
 git commit -m "chore(release): prepare for $version"
-git tag -am "$version" "$version"
+git -c core.commentChar="@" tag -am "$(git cliff --latest --strip header)" "$version"
