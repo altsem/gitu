35eb11435947689f7e6b3e19179efe29bff1fce6
Danil Menkin <gundarixdan@gmail.com>
2024-03-16 11:56:00 +0700

    Save pop and apply stashes without arguments

--- src/ops/mod.rs
+++ src/ops/mod.rs
@@ -19,6 +19,7 @@ pub(crate) mod reset;
 pub(crate) mod show;
 pub(crate) mod show_refs;
 pub(crate) mod stage;
+pub(crate) mod stash;
 pub(crate) mod unstage;
 
 pub(crate) type Action = Rc<dyn FnMut(&mut State, &mut Term) -> Res<()>>;
@@ -57,6 +58,11 @@ pub(crate) enum Op {
     RebaseAbort,
     RebaseContinue,
     ShowRefs,
+    Stash,
+    StashApply,
+    StashIndex,
+    StashKeepIndex,
+    StashPop,
 
     CommitFixup,
     Discard,
@@ -87,6 +93,7 @@ pub(crate) enum SubmenuOp {
     Push,
     Rebase,
     Reset,
+    Stash,
 }
 
 impl Op {
@@ -112,6 +119,11 @@ impl Op {
             Op::RebaseAbort => Box::new(rebase::RebaseAbort),
             Op::RebaseContinue => Box::new(rebase::RebaseContinue),
             Op::ShowRefs => Box::new(show_refs::ShowRefs),
+            Op::Stash => Box::new(stash::Stash),
+            Op::StashApply => Box::new(stash::StashApply),
+            Op::StashIndex => Box::new(stash::StashIndex),
+            Op::StashKeepIndex => Box::new(stash::StashKeepIndex),
+            Op::StashPop => Box::new(stash::StashPop),
 
             Op::CommitFixup => Box::new(commit::CommitFixup),
             Op::Discard => Box::new(discard::Discard),
@@ -142,6 +154,7 @@ impl Display for SubmenuOp {
             SubmenuOp::Push => "Push",
             SubmenuOp::Rebase => "Rebase",
             SubmenuOp::Reset => "Reset",
+            SubmenuOp::Stash => "Stash",
         })
     }
 }
---
+++ src/ops/stash.rs
@@ -0,0 +1,94 @@
+use super::{Action, OpTrait};
+use crate::{items::TargetData, state::State, term::Term};
+use derive_more::Display;
+use std::{process::Command, rc::Rc};
+
+#[derive(Default, Clone, Copy, PartialEq, Eq, Debug, Display)]
+#[display(fmt = "Stash")]
+pub(crate) struct Stash;
+impl OpTrait for Stash {
+    fn get_action(&self, _target: Option<&TargetData>) -> Option<Action> {
+        Some(Rc::new(|state: &mut State, term: &mut Term| {
+            let mut cmd = Command::new("git");
+            cmd.args(["stash", "--include-untracked"]);
+
+            state.issue_subscreen_command(term, cmd)?;
+            Ok(())
+        }))
+    }
+}
+
+#[derive(Default, Clone, Copy, PartialEq, Eq, Debug, Display)]
+#[display(fmt = "Stash index")]
+pub(crate) struct StashIndex;
+impl OpTrait for StashIndex {
+    fn get_action(&self, _target: Option<&TargetData>) -> Option<Action> {
+        Some(Rc::new(|state: &mut State, term: &mut Term| {
+            let mut cmd = Command::new("git");
+            cmd.args(["stash", "--staged"]);
+
+            state.issue_subscreen_command(term, cmd)?;
+            Ok(())
+        }))
+    }
+}
+
+#[derive(Default, Clone, Copy, PartialEq, Eq, Debug, Display)]
+#[display(fmt = "Stash keeping index")]
+pub(crate) struct StashKeepIndex;
+impl OpTrait for StashKeepIndex {
+    fn get_action(&self, _target: Option<&TargetData>) -> Option<Action> {
+        Some(Rc::new(|state: &mut State, term: &mut Term| {
+            let mut cmd = Command::new("git");
+            cmd.args(["stash", "--keep-index", "--include-untracked"]);
+
+            state.issue_subscreen_command(term, cmd)?;
+            Ok(())
+        }))
+    }
+}
+
+#[derive(Default, Clone, Copy, PartialEq, Eq, Debug, Display)]
+#[display(fmt = "Pop stash")]
+pub(crate) struct StashPop;
+impl OpTrait for StashPop {
+    fn get_action(&self, _target: Option<&TargetData>) -> Option<Action> {
+        Some(Rc::new(|state: &mut State, term: &mut Term| {
+            let mut cmd = Command::new("git");
+            cmd.args(["stash", "pop"]);
+
+            state.issue_subscreen_command(term, cmd)?;
+            Ok(())
+        }))
+    }
+}
+
+#[derive(Default, Clone, Copy, PartialEq, Eq, Debug, Display)]
+#[display(fmt = "Apply stash")]
+pub(crate) struct StashApply;
+impl OpTrait for StashApply {
+    fn get_action(&self, _target: Option<&TargetData>) -> Option<Action> {
+        Some(Rc::new(|state: &mut State, term: &mut Term| {
+            let mut cmd = Command::new("git");
+            cmd.args(["stash", "apply"]);
+
+            state.issue_subscreen_command(term, cmd)?;
+            Ok(())
+        }))
+    }
+}
+
+#[derive(Default, Clone, Copy, PartialEq, Eq, Debug, Display)]
+#[display(fmt = "Drop stash")]
+pub(crate) struct StashDrop;
+impl OpTrait for StashDrop {
+    fn get_action(&self, _target: Option<&TargetData>) -> Option<Action> {
+        Some(Rc::new(|state: &mut State, term: &mut Term| {
+            let mut cmd = Command::new("git");
+            cmd.args(["stash", "drop"]);
+
+            state.issue_subscreen_command(term, cmd)?;
+            Ok(())
+        }))
+    }
+}
