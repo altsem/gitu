680777bcfc7c8d83ce598d174e0e1792cded3720
altsem <alltidsemester@pm.me>
2024-04-07 23:32:02 +0200

    feat: add --prune and --tags flags to Fetch menu

--- src/ops/fetch.rs
+++ src/ops/fetch.rs
@@ -3,7 +3,10 @@ use crate::{items::TargetData, menu::arg::Arg};
 use derive_more::Display;
 use std::{process::Command, rc::Rc};
 
-pub(crate) const ARGS: &[Arg] = &[];
+pub(crate) const ARGS: &[Arg] = &[
+    Arg::new("--prune", "Prune deleted branches", false),
+    Arg::new("--tags", "Fetch all tags", false),
+];
 
 #[derive(Default, Clone, Copy, PartialEq, Eq, Debug, Display)]
 #[display(fmt = "Fetch all")]
@@ -13,6 +16,7 @@ impl OpTrait for FetchAll {
         Some(Rc::new(|state, term| {
             let mut cmd = Command::new("git");
             cmd.args(["fetch", "--all", "--jobs", "10"]);
+            cmd.args(state.pending_menu.as_ref().unwrap().args());
 
             state.run_cmd_async(term, &[], cmd)?;
             Ok(())
