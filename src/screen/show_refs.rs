3e830c5b44856e5f7c156611e56752065f4029d1
Danil Menkin <gundarixdan@gmail.com>
2024-04-14 15:42:47 +0700

    feat: make 3 sections in show refs screen: branches, remotes, tags

--- src/screen/show_refs.rs
+++ src/screen/show_refs.rs
@@ -2,11 +2,11 @@ use std::{iter, rc::Rc};
 
 use super::Screen;
 use crate::{
-    config::Config,
+    config::{Config, StyleConfigEntry},
     items::{Item, TargetData},
     Res,
 };
-use git2::{BranchType, Repository};
+use git2::{BranchType, Reference, Repository};
 use ratatui::{
     prelude::Rect,
     text::{Line, Span},
@@ -18,50 +18,40 @@ pub(crate) fn create(config: Rc<Config>, repo: Rc<Repository>, size: Rect) -> Re
         size,
         Box::new(move || {
             let style = &config.style;
-            let head = repo.head().ok();
 
             Ok(iter::once(Item {
-                id: "branches".into(),
+                id: "local_branches".into(),
                 display: Line::styled("Branches".to_string(), &style.section_header),
                 section: true,
                 depth: 0,
                 ..Default::default()
             })
+            .chain(branches(&repo, Some(BranchType::Local), &style.branch)?)
+            .chain(iter::once(Item {
+                id: "remote_branches".into(),
+                display: Line::styled("Remote".to_string(), &style.section_header),
+                section: true,
+                depth: 0,
+                ..Default::default()
+            }))
+            .chain(branches(&repo, Some(BranchType::Remote), &style.branch)?)
+            .chain(iter::once(Item {
+                id: "tags".into(),
+                display: Line::styled("Tags".to_string(), &style.section_header),
+                section: true,
+                depth: 0,
+                ..Default::default()
+            }))
             .chain(
-                repo.branches(Some(BranchType::Local))?
+                repo.references()?
                     .filter_map(Result::ok)
-                    .map(|(branch, _branch_type)| {
-                        let name = Span::styled(
-                            branch.name().unwrap().unwrap().to_string(),
-                            &style.branch,
-                        );
-
-                        let prefix = Span::raw(
-                            if branch.get().name() == head.as_ref().and_then(|h| h.name()) {
-                                "* "
-                            } else {
-                                "  "
-                            },
-                        );
-
-                        let upstream_name = if let Ok(upstream) = branch.upstream() {
-                            if let Ok(Some(name)) = upstream.name() {
-                                Span::styled(name.to_string(), &style.remote)
-                            } else {
-                                Span::raw("")
-                            }
-                        } else {
-                            Span::raw("")
-                        };
+                    .filter(Reference::is_tag)
+                    .map(|tag| {
+                        let name = Span::styled(tag.name().unwrap().to_string(), &style.branch);
 
                         Item {
                             id: name.clone().content,
-                            display: Line::from(vec![
-                                prefix,
-                                name.clone(),
-                                Span::raw("   "),
-                                upstream_name,
-                            ]),
+                            display: Line::from(vec![prefix(repo.head().ok(), &tag), name.clone()]),
                             depth: 1,
                             target_data: Some(TargetData::Branch(name.content.into())),
                             ..Default::default()
@@ -70,5 +60,36 @@ pub(crate) fn create(config: Rc<Config>, repo: Rc<Repository>, size: Rect) -> Re
             )
             .collect())
         }),
+    )
+}
+
+fn branches<'a>(
+    repo: &'a Repository,
+    filter: Option<BranchType>,
+    style: &'a StyleConfigEntry,
+) -> Res<impl Iterator<Item = Item> + 'a> {
+    Ok(repo
+        .branches(filter)?
+        .filter_map(Result::ok)
+        .map(move |(branch, _branch_type)| {
+            let name = Span::styled(branch.name().unwrap().unwrap().to_string(), style);
+
+            Item {
+                id: name.clone().content,
+                display: Line::from(vec![prefix(repo.head().ok(), branch.get()), name.clone()]),
+                depth: 1,
+                target_data: Some(TargetData::Branch(name.content.into())),
+                ..Default::default()
+            }
+        }))
+}
+
+fn prefix(head: Option<Reference>, target: &Reference) -> Span<'static> {
+    Span::raw(
+        if target.target() == head.as_ref().and_then(Reference::target) {
+            "* "
+        } else {
+            "  "
+        },
     )
 }
