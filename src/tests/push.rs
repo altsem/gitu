76628324b6b66048fcbba8d8737e3fdbf4f2ee7f
Radek Zikmund <r.zikmund.rz@gmail.com>
2024-05-01 21:57:06 +0200

    Add tests

--- src/tests/push.rs
+++ src/tests/push.rs
@@ -20,3 +20,13 @@ fn open_push_menu_after_dash_input() {
     commit(ctx.dir.path(), "new-file", "");
     snapshot!(ctx, "-P");
 }
+
+#[test]
+fn push_elsewhere_prompt() {
+    snapshot!(TestContext::setup_clone(), "Pe");
+}
+
+#[test]
+fn push_elsewhere() {
+    snapshot!(TestContext::setup_clone(), "Peorigin<enter>");
+}
