b8dbd2ca4e351a53d5e082781b7438ea5227d22c
Calum Sieppert <sieppertcalum@gmail.com>
2024-08-03 19:59:11 -0600

    Add more tests for set_upstream

--- src/tests/remote.rs
+++ src/tests/remote.rs
@@ -1,27 +1,73 @@
-use crate::git::remote::set_upstream;
-use crate::tests::helpers::RepoTestContext;
+use git2::{Buf, Error, Repository};
 
-// TODO: test that the remote is actually updated; test the different branches of `set_upstream`
+use crate::git::remote::set_upstream;
+use crate::tests::helpers::{commit, RepoTestContext};
+
+fn get_head_name(repo: &Repository) -> String {
+    repo.head().unwrap().name().unwrap().into()
+}
+
+fn get_branch_merge(repo: &Repository) -> Result<Buf, Error> {
+    repo.branch_upstream_name(&get_head_name(repo))
+}
+
+fn get_branch_merge_str(repo: &Repository) -> String {
+    get_branch_merge(repo).unwrap().as_str().unwrap().into()
+}
+
+fn get_branch_remote(repo: &Repository) -> Result<Buf, Error> {
+    repo.branch_upstream_remote(&get_head_name(repo))
+}
+
+fn get_branch_remote_str(repo: &Repository) -> String {
+    get_branch_remote(repo).unwrap().as_str().unwrap().into()
+}
+
+fn branch_from_head(repo: &Repository, name: impl AsRef<str>) -> git2::Branch {
+    repo
+        .branch(
+            name.as_ref(),
+            &repo.head().unwrap().peel_to_commit().unwrap(),
+            true,
+        )
+        .unwrap()
+}
+
+#[test]
+fn remove_upstream() {
+    let ctx = RepoTestContext::setup_init();
+
+    let repo = ctx.local_repo;
+    commit(repo.workdir().unwrap(), "file.txt", "content");
+
+    set_upstream(&repo, None).unwrap();
+
+    let actual_merge = get_branch_merge(&repo);
+
+    match actual_merge {
+        Ok(_) => panic!("Branch should not have upstream"),
+        Err(e) => assert!(e.message().ends_with("does not have an upstream"), "Actual: {}", e)
+    }
+
+    let actual_remote = get_branch_remote(&repo);
+
+    match actual_remote {
+        Ok(_) => panic!("Branch should not have remote upstream"),
+        Err(e) => assert!(e.message().ends_with("does not have an upstream remote"), "Actual: {}", e)
+    }
+}
+
 #[test]
 fn set_upstream_basic() {
     let ctx = RepoTestContext::setup_clone();
 
     let repo = ctx.local_repo;
-    let head = repo.head().unwrap();
-    let remote_name = repo.branch_upstream_remote(head.name().unwrap()).unwrap();
-    let remote_name = remote_name.as_str().unwrap();
+    let remote_name = get_branch_remote_str(&repo);
 
-    let upstream_branch = ctx
-        .remote_repo
-        .branch(
-            "upstream-branch",
-            &ctx.remote_repo.head().unwrap().peel_to_commit().unwrap(),
-            true,
-        )
-        .unwrap();
+    let upstream_branch = branch_from_head(&ctx.remote_repo, "upstream-branch");
     let upstream_ref = upstream_branch.into_reference();
 
-    repo.find_remote(remote_name)
+    repo.find_remote(&remote_name)
         .unwrap()
         .fetch(&[upstream_ref.shorthand().unwrap()], None, None)
         .unwrap();
@@ -37,11 +83,37 @@ fn set_upstream_basic() {
 
     set_upstream(&repo, Some(&upstream_ref)).unwrap();
 
-    let actual_merge = repo.branch_upstream_name(head.name().unwrap()).unwrap();
-    let actual_merge = actual_merge.as_str().unwrap();
-    let actual_remote = repo.branch_upstream_remote(head.name().unwrap()).unwrap();
-    let actual_remote = actual_remote.as_str().unwrap();
+    let actual_merge = get_branch_merge_str(&repo);
+    let actual_remote = get_branch_remote_str(&repo);
 
     assert_eq!(actual_merge, upstream_ref.name().unwrap());
     assert_eq!(actual_remote, remote_name);
+}
+
+#[test]
+fn set_upstream_local() {
+    let ctx = RepoTestContext::setup_init();
+
+    let repo = ctx.local_repo;
+    commit(repo.workdir().unwrap(), "file.txt", "content");
+
+    let upstream_branch = branch_from_head(&repo, "upstream-branch");
+    let upstream_ref = upstream_branch.into_reference();
+
+    // let upstream_name = upstream_ref.shorthand().unwrap();
+    // let upstream_ref = repo
+    //     .find_branch(
+    //         upstream_name,
+    //         git2::BranchType::Local,
+    //     )
+    //     .unwrap()
+    //     .into_reference();
+    // 
+    set_upstream(&repo, Some(&upstream_ref)).unwrap();
+
+    let actual_merge = get_branch_merge_str(&repo);
+    let actual_remote = get_branch_remote_str(&repo);
+
+    assert_eq!(actual_merge, upstream_ref.name().unwrap());
+    assert_eq!(actual_remote, ".");
 }
