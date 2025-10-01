altsem/gitu

commit 1d468ae0b7b5305d9014e5b44a1a819997be14ac

refactor: remove unnecessary double-input-polling

src/app.rs
@@ -152,10 +152,8 @@ impl App {
     }
 
     pub fn update(&mut self, term: &mut Term) -> Res<()> {
-        if term.backend_mut().poll_event(Duration::ZERO)? {
-            let event = term.backend_mut().read_event()?;
-            self.handle_event(term, event)?;
-        }
+        let event = term.backend_mut().read_event()?;
+        self.handle_event(term, event)?;
 
         if let Some(file_watcher) = &mut self.state.file_watcher {
             if file_watcher.pending_updates() {

commit a00f4899147b096652e363774998b8d9c2e93e83

undo bug introduced recently in 1d468ae0b7b5305d9014e5b44a1a819997be14ac

src/app.rs
@@ -144,16 +144,22 @@ impl App {
 
     pub fn run(&mut self, term: &mut Term, max_tick_delay: Duration) -> Res<()> {
         while !self.state.quit {
-            term.backend_mut().poll_event(max_tick_delay)?;
-            self.update(term)?;
+            let event = if term.backend_mut().poll_event(max_tick_delay)? {
+                Some(term.backend_mut().read_event()?)
+            } else {
+                None
+            };
+
+            self.update(term, event)?;
         }
 
         Ok(())
     }
 
-    pub fn update(&mut self, term: &mut Term) -> Res<()> {
-        let event = term.backend_mut().read_event()?;
-        self.handle_event(term, event)?;
+    pub fn update(&mut self, term: &mut Term, event: Option<Event>) -> Res<()> {
+        if let Some(e) = event {
+            self.handle_event(term, e)?;
+        }
 
         if let Some(file_watcher) = &mut self.state.file_watcher
             && file_watcher.pending_updates()