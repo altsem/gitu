altsem/gitu

commit a4588fe76578d17a1dba4b8fa83866c9adf7758c

fix(config): Report invalid key binding errors
Also do a bunch of clippy lints.

src/key_parser.rs
@@ -127,7 +127,7 @@ fn parse_normal_key(input: &str) -> IResult<&str, KeyCode> {
 }
 
 fn parse_char_key(input: &str) -> IResult<&str, KeyCode> {
-    map(anychar, |c| KeyCode::Char(c))(input)
+    map(anychar, KeyCode::Char)(input)
 }
 
 fn parse_special_key(input: &str) -> IResult<&str, KeyCode> {

src/app.rs
@@ -86,7 +86,7 @@ impl App {
         let pending_menu = root_menu(&config).map(PendingMenu::init);
 
         let clipboard = Clipboard::new()
-            .inspect_err(|e| log::warn!("Couldn't initialize clipboard: {}", e))
+            .inspect_err(|e| log::warn!("Couldn't initialize clipboard: {e}"))
             .ok();
 
         let mut app = Self {
@@ -369,7 +369,7 @@ impl App {
             return Ok(());
         };
 
-        log::debug!("pending cmd finished with {:?}", status);
+        log::debug!("pending cmd finished with {status:?}");
 
         let result = write_child_output_to_log(log_rwlock, child, status);
         self.state.pending_cmd = None;

src/cmd_log.rs
@@ -91,14 +91,11 @@ pub(crate) fn format_log_entry<'a>(
         }))
         .collect::<Vec<_>>(),
         CmdLogEntry::Error(err) => {
-            vec![Line::styled(
-                format!("! {}", err),
-                Style::new().red().bold(),
-            )]
+            vec![Line::styled(format!("! {err}"), Style::new().red().bold())]
         }
         CmdLogEntry::Info(msg) => {
             vec![Line::styled(
-                format!("> {}", msg),
+                format!("> {msg}"),
                 Style::new().green().bold(),
             )]
         }

src/config.rs
@@ -195,9 +195,9 @@ pub(crate) fn init_config(path: Option<PathBuf>) -> Res<Config> {
     let config_path = path.unwrap_or_else(config_path);
 
     if config_path.exists() {
-        log::info!("Loading config file at {:?}", config_path);
+        log::info!("Loading config file at {config_path:?}");
     } else {
-        log::info!("No config file at {:?}", config_path);
+        log::info!("No config file at {config_path:?}");
     }
 
     let FigmentConfig {

src/error.rs
@@ -60,48 +60,46 @@ impl std::error::Error for Error {}
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Error::StashList(e) => f.write_fmt(format_args!("Couldn't list stash: {}", e)),
-            Error::ReadLog(e) => f.write_fmt(format_args!("Couldn't read log: {}", e)),
+            Error::StashList(e) => f.write_fmt(format_args!("Couldn't list stash: {e}")),
+            Error::ReadLog(e) => f.write_fmt(format_args!("Couldn't read log: {e}")),
             Error::OpenRepo(e) => match e.code() {
                 git2::ErrorCode::NotFound => f.write_str("No .git found in the current directory"),
                 _ => f.write_fmt(format_args!("Couldn't open repo: {e:?}")),
             },
-            Error::FindGitDir(e) => f.write_fmt(format_args!("Couldn't find git directory: {}", e)),
-            Error::Term(e) => f.write_fmt(format_args!("Terminal error: {}", e)),
-            Error::Termwiz(e) => f.write_fmt(format_args!("Terminal error: {}", e)),
+            Error::FindGitDir(e) => f.write_fmt(format_args!("Couldn't find git directory: {e}")),
+            Error::Term(e) => f.write_fmt(format_args!("Terminal error: {e}")),
+            Error::Termwiz(e) => f.write_fmt(format_args!("Terminal error: {e}")),
             Error::GitDirUtf8(_e) => f.write_str("Git directory not valid UTF-8"),
-            Error::Config(e) => f.write_fmt(format_args!("Configuration error: {}", e)),
+            Error::Config(e) => f.write_fmt(format_args!("Configuration error: {e}")),
             Error::Bindings { bad_key_bindings } => {
                 let mut error_string = String::from("Errors while parsing key bindings:");
                 for item in bad_key_bindings {
                     error_string.push_str(&format!("\n{item}"));
                 }
-                f.write_fmt(format_args!("{}", error_string))
+                f.write_fmt(format_args!("{error_string}"))
             }
-            Error::FileWatcher(e) => f.write_fmt(format_args!("File watcher error: {}", e)),
+            Error::FileWatcher(e) => f.write_fmt(format_args!("File watcher error: {e}")),
             Error::ReadRebaseStatusFile(e) => {
-                f.write_fmt(format_args!("Couldn't read rebase status file: {}", e))
+                f.write_fmt(format_args!("Couldn't read rebase status file: {e}"))
             }
-            Error::ReadBranchName(e) => {
-                f.write_fmt(format_args!("Couldn't read branch name: {}", e))
-            }
+            Error::ReadBranchName(e) => f.write_fmt(format_args!("Couldn't read branch name: {e}")),
             Error::BranchNameUtf8(_e) => f.write_str("Branch name error"),
-            Error::GitDiff(e) => f.write_fmt(format_args!("Git diff error: {}", e)),
+            Error::GitDiff(e) => f.write_fmt(format_args!("Git diff error: {e}")),
             Error::GitDiffUtf8(e) => {
-                f.write_fmt(format_args!("Git diff output is not valid UTF-8: {}", e))
+                f.write_fmt(format_args!("Git diff output is not valid UTF-8: {e}"))
             }
-            Error::GitShow(e) => f.write_fmt(format_args!("Git show error: {}", e)),
+            Error::GitShow(e) => f.write_fmt(format_args!("Git show error: {e}")),
             Error::GitShowUtf8(e) => {
-                f.write_fmt(format_args!("Git show output is not valid UTF-8: {}", e))
+                f.write_fmt(format_args!("Git show output is not valid UTF-8: {e}"))
             }
-            Error::GitShowMeta(e) => f.write_fmt(format_args!("Git show metadata error: {}", e)),
+            Error::GitShowMeta(e) => f.write_fmt(format_args!("Git show metadata error: {e}")),
             Error::NotOnBranch => f.write_str("Head is not a branch"),
-            Error::GetHead(e) => f.write_fmt(format_args!("Couldn't get HEAD: {}", e)),
+            Error::GetHead(e) => f.write_fmt(format_args!("Couldn't get HEAD: {e}")),
             Error::CurrentBranchName(e) => {
-                f.write_fmt(format_args!("Couldn't get current branch name: {}", e))
+                f.write_fmt(format_args!("Couldn't get current branch name: {e}"))
             }
             Error::GetCurrentBranchUpstream(e) => {
-                f.write_fmt(format_args!("Couldn't get current branch upstream: {}", e))
+                f.write_fmt(format_args!("Couldn't get current branch upstream: {e}"))
             }
             Error::GetCurrentBranchUpstreamUtf8(_e) => {
                 f.write_str("Current branch upstream is not valid UTF-8")
@@ -110,31 +108,31 @@ impl Display for Error {
             Error::CannotDeleteCurrentBranch => f.write_str("Cannot delete current branch"),
             Error::BranchNameRequired => f.write_str("Branch name required"),
             Error::IsBranchMerged(e) => {
-                f.write_fmt(format_args!("Couldn't check if branch is merged: {}", e))
+                f.write_fmt(format_args!("Couldn't check if branch is merged: {e}"))
             }
-            Error::GetRemote(e) => f.write_fmt(format_args!("Couldn't get remote: {}", e)),
-            Error::ReadGitConfig(e) => f.write_fmt(format_args!("Couldn't read git config: {}", e)),
+            Error::GetRemote(e) => f.write_fmt(format_args!("Couldn't get remote: {e}")),
+            Error::ReadGitConfig(e) => f.write_fmt(format_args!("Couldn't read git config: {e}")),
             Error::ReadGitConfigUtf8(_e) => f.write_str("Git config is not valid UTF-8"),
             Error::DeleteGitConfig(e) => {
-                f.write_fmt(format_args!("Couldn't delete git config: {}", e))
+                f.write_fmt(format_args!("Couldn't delete git config: {e}"))
             }
-            Error::SetGitConfig(e) => f.write_fmt(format_args!("Couldn't set git config: {}", e)),
+            Error::SetGitConfig(e) => f.write_fmt(format_args!("Couldn't set git config: {e}")),
             Error::RemoteHasNoName => f.write_str("Remote has no name"),
-            Error::ReadOid(e) => f.write_fmt(format_args!("Couldn't read OID: {}", e)),
+            Error::ReadOid(e) => f.write_fmt(format_args!("Couldn't read OID: {e}")),
             Error::ArgMustBePositiveNumber => f.write_str("Value must be a number greater than 0"),
-            Error::ArgInvalidRegex(e) => f.write_fmt(format_args!("Invalid regex: {}", e)),
-            Error::Clipboard(e) => f.write_fmt(format_args!("Clipboard error: {}", e)),
-            Error::FindGitRev(e) => f.write_fmt(format_args!("Couldn't find git revision: {}", e)),
+            Error::ArgInvalidRegex(e) => f.write_fmt(format_args!("Invalid regex: {e}")),
+            Error::Clipboard(e) => f.write_fmt(format_args!("Clipboard error: {e}")),
+            Error::FindGitRev(e) => f.write_fmt(format_args!("Couldn't find git revision: {e}")),
             Error::NoEditorSet => f.write_fmt(format_args!(
                 "No editor environment variable set ({})",
                 crate::ops::show::EDITOR_VARS.join(", ")
             )),
-            Error::GitStatus(e) => f.write_fmt(format_args!("Git status error: {}", e)),
+            Error::GitStatus(e) => f.write_fmt(format_args!("Git status error: {e}")),
             Error::CmdAlreadyRunning => f.write_str("A command is already running"),
             Error::StashWorkTreeEmpty => f.write_str("Cannot stash: working tree is empty"),
-            Error::CouldntAwaitCmd(e) => f.write_fmt(format_args!("Couldn't await command: {}", e)),
+            Error::CouldntAwaitCmd(e) => f.write_fmt(format_args!("Couldn't await command: {e}")),
             Error::NoRepoWorkdir => f.write_str("No repository working directory"),
-            Error::SpawnCmd(e) => f.write_fmt(format_args!("Failed to spawn command: {}", e)),
+            Error::SpawnCmd(e) => f.write_fmt(format_args!("Failed to spawn command: {e}")),
             Error::CmdBadExit(args, code) => f.write_fmt(format_args!(
                 "'{}' exited with code: {}",
                 args,
@@ -142,12 +140,12 @@ impl Display for Error {
                     .unwrap_or_else(|| "".to_string())
             )),
             Error::CouldntReadCmdOutput(e) => {
-                f.write_fmt(format_args!("Couldn't read command output: {}", e))
+                f.write_fmt(format_args!("Couldn't read command output: {e}"))
             }
             Error::ListGitReferences(e) => {
-                f.write_fmt(format_args!("Couldn't list git references: {}", e))
+                f.write_fmt(format_args!("Couldn't list git references: {e}"))
             }
-            Error::OpenLogFile(e) => f.write_fmt(format_args!("Couldn't open log file: {}", e)),
+            Error::OpenLogFile(e) => f.write_fmt(format_args!("Couldn't open log file: {e}")),
             Error::PromptAborted => f.write_str("Aborted"),
             Error::NoMoreEvents => unimplemented!(),
         }

src/git/diff.rs
@@ -78,14 +78,14 @@ impl Diff {
                 } else if line.starts_with(add) {
                     None
                 } else if let Some(stripped) = line.strip_prefix(remove) {
-                    Some(format!(" {}", stripped))
+                    Some(format!(" {stripped}"))
                 } else {
                     Some(line.to_string())
                 }
             })
             .collect::<String>();
 
-        format!("{}{}{}", file_header, hunk_header, modified_content)
+        format!("{file_header}{hunk_header}{modified_content}")
     }
 
     pub(crate) fn file_line_of_first_diff(&self, file_i: usize, hunk_i: usize) -> usize {

src/git/mod.rs
@@ -188,7 +188,7 @@ pub(crate) fn show_summary(repo: &Repository, reference: &str) -> Res<Commit> {
     let email = commit
         .author()
         .email()
-        .map(|email| format!("<{}>", email))
+        .map(|email| format!("<{email}>"))
         .unwrap_or("".to_string());
 
     let message = commit
@@ -196,7 +196,7 @@ pub(crate) fn show_summary(repo: &Repository, reference: &str) -> Res<Commit> {
         .unwrap_or("")
         .to_string()
         .lines()
-        .map(|line| format!("    {}", line))
+        .map(|line| format!("    {line}"))
         .join("\n");
 
     let offset = chrono::FixedOffset::east_opt(author.when().offset_minutes() * 60).unwrap();

src/git/remote.rs
@@ -54,7 +54,7 @@ pub(crate) fn get_upstream_components(repo: &Repository) -> Res<Option<(String, 
         .map_err(Utf8Error::String)
         .map_err(Error::RemoteNameUtf8)?;
 
-        let remote_prefix = format!("{}/", remote);
+        let remote_prefix = format!("{remote}/");
         Ok(Some((remote, branch.replace(&remote_prefix, ""))))
     } else {
         Ok(Some((".".into(), branch)))

commit 9737a94c6faba043497c28a2cfd1c99f9d7bc0b3

feat(config): recognize "space" special character in key parser
This addresses a minor regression in #407 which rendered the following invalid:

    [bindings.root]
    stage = [" "]

Rather than removing the trimming of the key code string, this commit fixes the
problem by recognizing "space" specially, similar to e.g. "tab". As a result,
the above config can be achieved with:

    [bindings.root]
    stage = ["space"]

src/key_parser.rs
@@ -153,6 +153,7 @@ fn parse_special_key(input: &str) -> IResult<&str, KeyCode> {
             map(tag("end"), |_| KeyCode::End),
             map(tag("pageup"), |_| KeyCode::PageUp),
             map(tag("pagedown"), |_| KeyCode::PageDown),
+            map(tag("space"), |_| KeyCode::Char(' ')),
             map(tag("tab"), |_| KeyCode::Tab),
             map(tag("delete"), |_| KeyCode::Delete),
             map(tag("insert"), |_| KeyCode::Insert),
@@ -208,6 +209,10 @@ mod tests {
         assert_eq!(
             parse_key_combo("enter"),
             Ok(("", (KeyModifiers::NONE, vec![KeyCode::Enter])))
+        );
+        assert_eq!(
+            parse_key_combo("space"),
+            Ok(("", (KeyModifiers::NONE, vec![KeyCode::Char(' ')])))
         );
     }
 