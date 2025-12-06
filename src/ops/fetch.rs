b5123ea1ecdfdd952adb54a13ae7e67a848dc884
Derek Passen <dpassen1@gmail.com>
2025-12-06 13:25:21 -0600

    feat: Add fetch from upstream and push remote

--- src/ops/fetch.rs
+++ src/ops/fetch.rs
@@ -2,6 +2,8 @@ use super::{Action, OpTrait};
 use crate::{
     Res,
     app::{App, PromptParams, State},
+    error::Error,
+    git::remote::{get_push_remote, get_upstream_components, get_upstream_remote, set_push_remote},
     item_data::ItemData,
     menu::arg::Arg,
     term::Term,
@@ -65,4 +67,93 @@ fn fetch_elsewhere(app: &mut App, term: &mut Term, remote: &str) -> Res<()> {
     app.close_menu();
     app.run_cmd_async(term, &[], cmd)?;
     Ok(())
+}
+
+fn set_upstream_and_fetch(app: &mut App, term: &mut Term, upstream_name: &str) -> Res<()> {
+    let mut cmd = Command::new("git");
+    cmd.args(["branch", "--set-upstream-to", upstream_name]);
+    app.run_cmd(term, &[], cmd)?;
+
+    let Some((remote, _branch)) = get_upstream_components(&app.state.repo)? else {
+        return Ok(());
+    };
+
+    fetch_elsewhere(app, term, &remote)
+}
+
+fn set_push_remote_and_fetch(app: &mut App, term: &mut Term, push_remote_name: &str) -> Res<()> {
+    let repo = app.state.repo.clone();
+    let push_remote = repo
+        .find_remote(push_remote_name)
+        .map_err(Error::GetRemote)?;
+
+    // TODO Would be nice to have the command visible in the log. Resort to `git config`?
+    set_push_remote(&repo, Some(&push_remote))?;
+
+    fetch_elsewhere(app, term, push_remote_name)
+}
+
+pub(crate) struct FetchPushRemote;
+impl OpTrait for FetchPushRemote {
+    fn get_action(&self, _target: &ItemData) -> Option<Action> {
+        Some(Rc::new(
+            |app: &mut App, term: &mut Term| match get_push_remote(&app.state.repo)? {
+                None => {
+                    let push_remote_name = app.prompt(
+                        term,
+                        &PromptParams {
+                            prompt: "Set pushRemote then fetch",
+                            ..Default::default()
+                        },
+                    )?;
+
+                    set_push_remote_and_fetch(app, term, &push_remote_name)?;
+                    Ok(())
+                }
+                Some(push_remote) => fetch_elsewhere(app, term, &push_remote),
+            },
+        ))
+    }
+
+    fn display(&self, state: &State) -> String {
+        match get_push_remote(&state.repo) {
+            Ok(Some(remote)) => format!("from {remote}"),
+            Ok(None) => "from pushRemote, setting that".into(),
+            Err(e) => format!("error: {e}"),
+        }
+    }
+}
+
+pub(crate) struct FetchUpstream;
+impl OpTrait for FetchUpstream {
+    fn get_action(&self, _target: &ItemData) -> Option<Action> {
+        Some(Rc::new(
+            |app: &mut App, term: &mut Term| match get_upstream_components(&app.state.repo)? {
+                None => {
+                    let mut prompt = Rc::new(move |app: &mut App, term: &mut Term| {
+                        let upstream_name = app.prompt(
+                            term,
+                            &PromptParams {
+                                prompt: "Set upstream then fetch",
+                                ..Default::default()
+                            },
+                        )?;
+
+                        set_upstream_and_fetch(app, term, &upstream_name)?;
+                        Ok(())
+                    });
+                    Rc::get_mut(&mut prompt).unwrap()(app, term)
+                }
+                Some((remote, _branch)) => fetch_elsewhere(app, term, &remote),
+            },
+        ))
+    }
+
+    fn display(&self, state: &State) -> String {
+        match get_upstream_remote(&state.repo) {
+            Ok(Some(upstream)) => format!("from {upstream}"),
+            Ok(None) => "from upstream, setting that".into(),
+            Err(e) => format!("error: {e}"),
+        }
+    }
 }
