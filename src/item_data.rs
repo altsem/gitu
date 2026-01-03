fb4f08cb85b1982f38d6853e49d05b72706f8436
Sean Billig <sean.billig@gmail.com>
2026-01-02 23:09:58 -0600

    use stash ref (stash@{N}) instead of commit hash as header on stash view

--- src/item_data.rs
+++ src/item_data.rs
@@ -87,6 +87,7 @@ pub(crate) enum SectionHeader {
     Stashes,
     RecentCommits,
     Commit(String),
+    StashRef(String),
     StagedChanges(usize),
     UnstagedChanges(usize),
     UntrackedFiles(usize),
